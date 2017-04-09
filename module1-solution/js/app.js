2de9a56e5b6c5320cebece00ba20cd2f81f517e2
hieumt <hieumt@coursera.com>
2017-04-09 16:17:35 +0700

    first commit assignment 2

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -2,30 +2,51 @@
   'use strict';
 
   angular.module('LunchCheck', [])
-  .controller('LunchCheckController', function($scope) {
-    $scope.menu = "";     // containing the entered string of menu
-    $scope.message = "";  // message will be showing under the button
+  .controller('LunchCheckController', LunchCheckController)
+  .filter('cust', CustomFilterFactory);
 
-    $scope.onCheckBtnClick = function() {
-      var inputMenu = $scope.menu;
-      var itemList = [];
+    LunchCheckController.inject = ['$scope', 'custFilter']
+    function LunchCheckController ($scope, custFilter) {
+      $scope.menu = "";     // containing the entered string of menu
+      $scope.message = "";  // message will be showing under the button
+      $scope.customStyle={};
 
-      if(inputMenu == "" || inputMenu == " ") {
-        $scope.message = "Please enter data first";
-        return;
-      }
+      $scope.onCheckBtnClick = function() {
+        var inputMenu = $scope.menu;
+        var itemList = [];
 
-      itemList = inputMenu.split(',');
-      if (itemList.length <= 3) {
-        $scope.message = "Enjoy!";
-      } else {
-        $scope.message = "Too much!";
-      }
+        if(inputMenu == "" || inputMenu == " ") {
+          $scope.message = "Please enter data first";
+          $scope.customStyle.style = {"color":"red"};
+          return;
+        }
+
+        itemList = inputMenu.split(',');
+        if (itemList.length <= 3) {
+          $scope.message = "Enjoy!";
+          $scope.customStyle.style = {"color":"blue"};
+        } else {
+          $scope.message = "Too much!";
+          $scope.customStyle.style = {"color":"red"};
+        }
+      };
+
+      $scope.displayNumeric = function() {
+        var totalNameValue = calculateNumericForString($scope.name);
+        $scope.totalValue = totalNameValue;
+      };
+
+      $scope.saymessage = function() {
+        return $scope.message;
+      };
+  }
+
+  function CustomFilterFactory() {
+    return function(input){
+      var changedInput = ""
+      return changedInput;
     };
+  }
 
-    $scope.displayNumeric = function() {
-      var totalNameValue = calculateNumericForString($scope.name);
-      $scope.totalValue = totalNameValue;
-    };
-  });
+
 })();
