ea21447b838dceb5e5c5fa518c30bf251e28c61a
Mai Trung Hieu <hieumt@necvietnam.hn>
2017-04-14 17:07:37 +0700

    start coding module3 assignment

---
+++ module3-solution/js/app.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  angular.module('MenuSearchModule', [])
+  .controller('MenuSearchController', MenuSearchController)
+  .service('MenuSearchService', MenuSearchService);
+
+
+  function MenuSearchController() {
+    var searchCtl = this;
+  };
+
+
+
+
+
+  function MenuSearchService() {
+    var service = this;
+
+  };
+
+})();
